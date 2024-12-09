3c27030844f0a5da33cb88a9905c93bd95ce2152
Artjoms Stukans <artjoms.stukans@gmail.com>
2024-12-09 14:15:05 +0100

    update

--- src/main/java/com/stukans/advent/_2024/day9/DiskFragmenter.java
+++ src/main/java/com/stukans/advent/_2024/day9/DiskFragmenter.java
@@ -3,21 +3,106 @@ package com.stukans.advent._2024.day9;
 import com.stukans.advent._2024.Puzzle;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DiskFragmenter extends Puzzle {
 
     @Override
     public long solution1(File file) {
-        long answer = 0;
-
         String[] load = load(file);
         if (load.length > 1) {
             throw new RuntimeException("Too many loads!");
         }
         char[] charArray = load[0].toCharArray();
 
+        List<Element> list = buildFragmentedView(charArray);
+        List<Element> fragmented = fragment(list);
+        long calculate = calculate(fragmented);
+        return calculate;
+    }
 
+    @Override
+    public long solution2(File file) {
+        String[] load = load(file);
+        if (load.length > 1) {
+            throw new RuntimeException("Too many loads!");
+        }
+        char[] charArray = load[0].toCharArray();
 
+        List<Element> list = buildFragmentedView(charArray);
+        List<Element> fragmented = fragment(list);
+        long calculate = calculate(fragmented);
+        return calculate;
+    }
+
+    record Element(int index, int value) {
+    }
+
+    protected long calculate(List<Element> elements) {
+        long answer = 0;
+        for (int i = 0; i < elements.size(); i++) {
+            Element element = elements.get(i);
+            if (element.value != -1) {
+                long answer1 = i * element.value();
+                answer += answer1;
+            }
+
+        }
         return answer;
     }
+
+    protected List<Element> fragment(final List<Element> list) {
+        final List<Element> fragmented = new ArrayList<>(list);
+
+        int freeSpaceLocation = 0;
+        for (int i = list.size() - 1; i >= freeSpaceLocation; i--) {
+            Element c = fragmented.get(i);
+            if (c.value() == -1) {
+                continue;
+            }
+            freeSpaceLocation = getNextFreeSlot(fragmented, freeSpaceLocation);
+            if (freeSpaceLocation > i) {
+                break;
+            }
+            Element empty = fragmented.get(freeSpaceLocation);
+            fragmented.set(freeSpaceLocation, c);
+            fragmented.set(i, empty);
+        }
+        return fragmented;
+    }
+
+    private static int getNextFreeSlot(List<Element> fragmented, int freeSpaceLocation) {
+        for (int i = freeSpaceLocation; i < fragmented.size(); i++) {
+            if (fragmented.get(i).value() == -1) {
+                return i;
+            }
+        }
+        throw new RuntimeException("Too many fragments!");
+    }
+
+    protected List<Element> buildFragmentedView(char[] charArray) {
+        List<Element> elements = new ArrayList<>(charArray.length);
+        int index = 0;
+        int valueIndex = 0;
+        for (int i = 0; i < charArray.length; i += 2) {
+            int fileBlockSpace = Character.getNumericValue(charArray[i]);
+            for (int j = 0; j < fileBlockSpace; j++) {
+                elements.add(new Element(index, valueIndex));
+                index++;
+            }
+            valueIndex++;
+            if (i + 1 < charArray.length) {
+                int freeSpaceBlockSpace = Character.getNumericValue(charArray[i + 1]);
+                for (int j = 0; j < freeSpaceBlockSpace; j++) {
+                    elements.add(new Element(index, -1));
+                    index++;
+                }
+            }
+
+            index++;
+        }
+        return elements;
+    }
+
 }
--- src/test/java/com/stukans/advent/_2024/day9/DiskFragmenterTest.java
+++ src/test/java/com/stukans/advent/_2024/day9/DiskFragmenterTest.java
@@ -3,6 +3,7 @@ package com.stukans.advent._2024.day9;
 import com.stukans.advent._2024.Puzzle;
 import com.stukans.advent._2024.day8.ResonantCollinearity;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -20,7 +21,7 @@ class DiskFragmenterTest {
     private static Stream<Arguments> inputs1() {
         return Stream.of(
                 Arguments.of(folder + "/test1.txt", 1928)
-                , Arguments.of(folder + "/input.txt", -1)
+                , Arguments.of(folder + "/input.txt", 6299243228569L)
 
         );
     }
@@ -32,14 +33,14 @@ class DiskFragmenterTest {
         File file = new File(Objects.requireNonNull(url).getFile());
         Assertions.assertTrue(file.exists());
 
-        Puzzle puzzle = new ResonantCollinearity();
+        Puzzle puzzle = new DiskFragmenter();
 
         Assertions.assertEquals(output, puzzle.solution1(file), "Solution 1 not correct");
     }
 
     private static Stream<Arguments> inputs2() {
         return Stream.of(
-                Arguments.of(folder + "/test1.txt", -1)
+                Arguments.of(folder + "/test1.txt", 2858)
                 , Arguments.of(folder + "/input.txt", -1)
 
         );
@@ -52,9 +53,42 @@ class DiskFragmenterTest {
         File file = new File(Objects.requireNonNull(url).getFile());
         Assertions.assertTrue(file.exists());
 
-        Puzzle puzzle = new ResonantCollinearity();
+        Puzzle puzzle = new DiskFragmenter();
 
         Assertions.assertEquals(output2, puzzle.solution2(file), "Solution 2 not correct");
     }
+
+//    private static Stream<Arguments> inputs3() {
+//        return Stream.of(
+//                Arguments.of("0..111....22222", "022111222......")
+//                , Arguments.of("00...111...2...333.44.5555.6666.777.888899", "0099811188827773336446555566..............")
+//
+//        );
+//    }
+//
+//    @ParameterizedTest
+//    @MethodSource("inputs3")
+//    void puzzleFragment(String input, String expected) throws IOException {
+//        DiskFragmenter puzzle = new DiskFragmenter();
+//
+//        StringBuilder builder = new StringBuilder(input);
+//        Assertions.assertEquals(expected, puzzle.fragment(builder).toString());
+//    }
+
+//    private static Stream<Arguments> inputs4() {
+//        return Stream.of(
+//                Arguments.of("12345", "0..111....22222")
+//                , Arguments.of("2333133121414131402", "00...111...2...333.44.5555.6666.777.888899")
+//
+//        );
+//    }
+//
+//    @ParameterizedTest
+//    @MethodSource("inputs4")
+//    void puzzleFragmentView(String input, String expected) throws IOException {
+//        DiskFragmenter puzzle = new DiskFragmenter();
+//        Assertions.assertEquals(expected, puzzle.buildFragmentedView(input.toCharArray()).toString());
+//    }
+
 
 }
