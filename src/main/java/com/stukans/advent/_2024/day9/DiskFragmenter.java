a8dcb793d018b5f2362e86b4671d419eb3fd9e64
Artjoms Stukans <artjoms.stukans@gmail.com>
2024-12-10 10:00:29 +0100

    update

--- src/main/java/com/stukans/advent/_2024/day9/DiskFragmenter.java
+++ src/main/java/com/stukans/advent/_2024/day9/DiskFragmenter.java
@@ -1,5 +1,6 @@
 package com.stukans.advent._2024.day9;
 
+import com.stukans.advent._2024.Pair;
 import com.stukans.advent._2024.Puzzle;
 
 import java.io.File;
@@ -16,8 +17,8 @@ public class DiskFragmenter extends Puzzle {
         }
         char[] charArray = load[0].toCharArray();
 
-        List<Element> list = buildFragmentedView(charArray);
-        List<Element> fragmented = fragment(list);
+        List<Integer> list = buildFragmentedView(charArray);
+        List<Integer> fragmented = fragment(list);
         long calculate = calculate(fragmented);
         return calculate;
     }
@@ -30,21 +31,25 @@ public class DiskFragmenter extends Puzzle {
         }
         char[] charArray = load[0].toCharArray();
 
-        List<Element> list = buildFragmentedView(charArray);
-        List<Element> fragmented = fragment(list);
+        List<Integer> list = buildFragmentedView(charArray);
+        List<Integer> fragmented = fragmentN(list);
         long calculate = calculate(fragmented);
         return calculate;
     }
 
     record Element(int index, int value) {
+        @Override
+        public String toString() {
+            return value + "";
+        }
     }
 
-    protected long calculate(List<Element> elements) {
+    protected long calculate(List<Integer> elements) {
         long answer = 0;
         for (int i = 0; i < elements.size(); i++) {
-            Element element = elements.get(i);
-            if (element.value != -1) {
-                long answer1 = i * element.value();
+            Integer element = elements.get(i);
+            if (element != -1) {
+                long answer1 = (long) i * element;
                 answer += answer1;
             }
 
@@ -52,55 +57,185 @@ public class DiskFragmenter extends Puzzle {
         return answer;
     }
 
-    protected List<Element> fragment(final List<Element> list) {
-        final List<Element> fragmented = new ArrayList<>(list);
+    protected List<Integer> fragment(final List<Integer> list) {
+        final List<Integer> fragmented = new ArrayList<>(list);
 
         int freeSpaceLocation = 0;
         for (int i = list.size() - 1; i >= freeSpaceLocation; i--) {
-            Element c = fragmented.get(i);
-            if (c.value() == -1) {
+            Integer c = fragmented.get(i);
+            if (c == -1) {
                 continue;
             }
             freeSpaceLocation = getNextFreeSlot(fragmented, freeSpaceLocation);
             if (freeSpaceLocation > i) {
                 break;
             }
-            Element empty = fragmented.get(freeSpaceLocation);
+            Integer empty = fragmented.get(freeSpaceLocation);
             fragmented.set(freeSpaceLocation, c);
             fragmented.set(i, empty);
         }
         return fragmented;
     }
 
-    private static int getNextFreeSlot(List<Element> fragmented, int freeSpaceLocation) {
+    protected List<Integer> fragmentN(final List<Integer> list) {
+        final List<Integer> fragmented = new ArrayList<>(list);
+        int from = fragmented.size() - 1;
+        //System.out.println(fragmented);
+        do {
+            Pair<Integer, Integer> filesBlock = findFileBlockToMove(fragmented, from);
+            int size = filesBlock.getRight() - filesBlock.getLeft() + 1;
+            Pair<Integer, Integer> freeBlock = findFreeBlockToTake(fragmented, size);
+
+
+            if (freeBlock != null && filesBlock.getLeft() > freeBlock.getLeft()) {
+                //System.out.println(filesBlock + "|" + freeBlock);
+                for (int i = freeBlock.getLeft(); i < freeBlock.getRight() + 1; i++) {
+                    fragmented.set(i, fragmented.get(filesBlock.getLeft()));
+                }
+                for (int i = filesBlock.getLeft(); i < filesBlock.getRight() + 1; i++) {
+                    fragmented.set(i, -1);
+                }
+            } else {
+                //System.out.println(filesBlock + "|" + freeBlock + " SKIP");
+            }
+            //System.out.println(fragmented);
+            from = filesBlock.getLeft() - 1;
+        } while (from >= 0);
+        return fragmented;
+    }
+
+    public Pair<Integer, Integer> findFileBlockToMove(List<Integer> fragmented, int from) {
+        int location = from;
+        Integer num = fragmented.get(location);
+        boolean blockStarted = false;
+        int blockStart = from;
+        int blockLength = 0;
+
+        int blockValue = -1;
+        boolean blockValueChanged = false;
+
+        do {
+            if (num != -1) {
+                if (blockValue == -1) {
+                    blockValue = num;
+                } else {
+                    if (blockValue != num) {
+                        blockValueChanged = true;
+                    }
+                }
+            }
+
+            if (blockStarted && (num == -1 || blockValueChanged)) {
+                return Pair.of(blockStart - blockLength + 1, blockStart);
+            }
+
+            if (num != -1) {
+                if (!blockStarted) {
+                    blockStart = location;
+                }
+
+                blockStarted = true;
+                blockLength++;
+            } else {
+                blockStarted = false;
+                blockStart = 0;
+                blockLength = 0;
+            }
+            location--;
+            if (location < 0) {
+                if (blockStarted == false) {
+                    return null;
+                } else {
+                    return Pair.of(0, blockStart);
+                }
+            }
+            num = fragmented.get(location);
+        } while (location < fragmented.size() - 1);
+
+        return null;
+    }
+
+    public Pair<Integer, Integer> findFreeBlockToTake(List<Integer> fragmented, int size) {
+        int location = 0;
+        Integer num = fragmented.get(location);
+        boolean freeSpaceStarted = false;
+        int freeSpaceStart = 0;
+        int freeSpaceLength = 0;
+
+        do {
+            if (num == -1) {
+                if (!freeSpaceStarted) {
+                    freeSpaceStart = location;
+                }
+
+                freeSpaceStarted = true;
+                freeSpaceLength++;
+
+                if (freeSpaceStarted && size <= freeSpaceLength) {
+                    return Pair.of(freeSpaceStart, freeSpaceStart + freeSpaceLength - 1);
+                }
+
+            } else {
+                freeSpaceStarted = false;
+                freeSpaceStart = 0;
+                freeSpaceLength = 0;
+            }
+            location++;
+            num = fragmented.get(location);
+        } while (location < fragmented.size() - 1);
+
+        return null;
+    }
+
+    private static List<List<Element>> mapToBlocks(List<Element> fragmented) {
+        List<List<Element>> blocks = new ArrayList<>();
+        List<Element> inner = new ArrayList<>();
+        for (int i = 0; i < fragmented.size(); i++) {
+            inner.add(fragmented.get(i));
+
+            if (i < fragmented.size() - 1 && fragmented.get(i).value() != fragmented.get(i + 1).value()) {
+                blocks.add(inner);
+                inner = new ArrayList<>();
+            }
+        }
+        blocks.add(inner);
+        return blocks;
+    }
+
+    private int getNextFreeSlot(List<Integer> fragmented, int freeSpaceLocation) {
         for (int i = freeSpaceLocation; i < fragmented.size(); i++) {
-            if (fragmented.get(i).value() == -1) {
+            if (fragmented.get(i) == -1) {
                 return i;
             }
         }
         throw new RuntimeException("Too many fragments!");
     }
 
-    protected List<Element> buildFragmentedView(char[] charArray) {
-        List<Element> elements = new ArrayList<>(charArray.length);
-        int index = 0;
+    private int getNextFreeSlotN(List<List<Element>> fragmented, int size) {
+        for (int i = 0; i < fragmented.size(); i++) {
+            List<Element> element = fragmented.get(i);
+            if (element.iterator().next().value() == -1 && size <= element.size()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    protected List<Integer> buildFragmentedView(char[] charArray) {
+        List<Integer> elements = new ArrayList<>(charArray.length);
         int valueIndex = 0;
         for (int i = 0; i < charArray.length; i += 2) {
             int fileBlockSpace = Character.getNumericValue(charArray[i]);
             for (int j = 0; j < fileBlockSpace; j++) {
-                elements.add(new Element(index, valueIndex));
-                index++;
+                elements.add(valueIndex);
             }
             valueIndex++;
             if (i + 1 < charArray.length) {
                 int freeSpaceBlockSpace = Character.getNumericValue(charArray[i + 1]);
                 for (int j = 0; j < freeSpaceBlockSpace; j++) {
-                    elements.add(new Element(index, -1));
-                    index++;
+                    elements.add(-1);
                 }
             }
-
-            index++;
         }
         return elements;
     }
--- src/test/java/com/stukans/advent/_2024/day9/DiskFragmenterTest.java
+++ src/test/java/com/stukans/advent/_2024/day9/DiskFragmenterTest.java
@@ -1,9 +1,8 @@
 package com.stukans.advent._2024.day9;
 
+import com.stukans.advent._2024.Pair;
 import com.stukans.advent._2024.Puzzle;
-import com.stukans.advent._2024.day8.ResonantCollinearity;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -11,6 +10,8 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.stream.Stream;
 
@@ -41,7 +42,7 @@ class DiskFragmenterTest {
     private static Stream<Arguments> inputs2() {
         return Stream.of(
                 Arguments.of(folder + "/test1.txt", 2858)
-                , Arguments.of(folder + "/input.txt", -1)
+                , Arguments.of(folder + "/input.txt", 6326952672104L)
 
         );
     }
@@ -58,37 +59,75 @@ class DiskFragmenterTest {
         Assertions.assertEquals(output2, puzzle.solution2(file), "Solution 2 not correct");
     }
 
-//    private static Stream<Arguments> inputs3() {
-//        return Stream.of(
-//                Arguments.of("0..111....22222", "022111222......")
-//                , Arguments.of("00...111...2...333.44.5555.6666.777.888899", "0099811188827773336446555566..............")
-//
-//        );
-//    }
-//
-//    @ParameterizedTest
-//    @MethodSource("inputs3")
-//    void puzzleFragment(String input, String expected) throws IOException {
-//        DiskFragmenter puzzle = new DiskFragmenter();
-//
-//        StringBuilder builder = new StringBuilder(input);
-//        Assertions.assertEquals(expected, puzzle.fragment(builder).toString());
-//    }
+    private static Stream<Arguments> inputs3() {
+        return Stream.of(
+                Arguments.of(List.of(1, 1, 1, -1, -1, 1, 1), 2, Pair.of(3, 4))
+                , Arguments.of(List.of(1, 1, 1, -1, -1, -1, 1), 2, Pair.of(3, 4))
+                , Arguments.of(List.of(1, 1, -1, -1, -1, -1, 1), 2, Pair.of(2, 3))
+                , Arguments.of(List.of(-1, 1, -1, -1, -1, -1, 1), 2, Pair.of(2, 3))
+                , Arguments.of(List.of(-1, -1, -1, -1, -1, -1, 1), 2, Pair.of(0, 1))
+                , Arguments.of(List.of(-1, -1, -1, 1, -1, -1, 1), 3, Pair.of(0, 2))
+                , Arguments.of(List.of(-1, -1, -1, 1, -1, -1, 1), 1, Pair.of(0, 0))
+                , Arguments.of(List.of(-1, -1, -1, 1, -1, -1, 1), 4, null)
 
-//    private static Stream<Arguments> inputs4() {
-//        return Stream.of(
-//                Arguments.of("12345", "0..111....22222")
-//                , Arguments.of("2333133121414131402", "00...111...2...333.44.5555.6666.777.888899")
-//
-//        );
-//    }
-//
-//    @ParameterizedTest
-//    @MethodSource("inputs4")
-//    void puzzleFragmentView(String input, String expected) throws IOException {
-//        DiskFragmenter puzzle = new DiskFragmenter();
-//        Assertions.assertEquals(expected, puzzle.buildFragmentedView(input.toCharArray()).toString());
-//    }
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputs3")
+    void findFreeBlockToTake(List<Integer> list, int size, Pair<Integer, Integer> expected) {
+        DiskFragmenter fragmenter = new DiskFragmenter();
+        Pair<Integer, Integer> pair = fragmenter.findFreeBlockToTake(list, size);
+        Assertions.assertEquals(expected, pair);
+
+    }
+
+    private static Stream<Arguments> inputs4() {
+        return Stream.of(
+                Arguments.of(List.of(1, 1, 1, -1, -1, 1, 1), Pair.of(5, 6))
+                , Arguments.of(List.of(1, 1, 1, -1, -1, -1, 1), Pair.of(6, 6))
+                , Arguments.of(List.of(1, 1, -1, -1, -1, 1, -1), Pair.of(5, 5))
+                , Arguments.of(List.of(-1, 1, -1, -1, 1, 1, -1), Pair.of(4, 5))
+                , Arguments.of(List.of(-1, -1, 1, 1, -1, -1, -1), Pair.of(2, 3))
+                , Arguments.of(List.of(-1, 1, 1, 1, 1, -1, -1), Pair.of(1, 4))
+                , Arguments.of(List.of(1, -1, -1, -1, -1, -1, -1), Pair.of(0, 0))
+                , Arguments.of(List.of(-1, -1, -1, -1, -1, -1, -1), null)
+
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputs4")
+    void findFileBlockToTake(List<Integer> list, Pair<Integer, Integer> expected) {
+        DiskFragmenter fragmenter = new DiskFragmenter();
+        Pair<Integer, Integer> pair = fragmenter.findFileBlockToMove(list, list.size() - 1);
+        Assertions.assertEquals(expected, pair);
+
+    }
+
+    private static Stream<Arguments> inputs5() {
+        return Stream.of(
+                Arguments.of(List.of(4, 3, 3, -1, 2, 1, 1),
+                        List.of(Pair.of(5, 6), Pair.of(4, 4), Pair.of(1, 2), Pair.of(0, 0)))
+
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputs5")
+    void findFileBlocksToTake(List<Integer> list, List<Pair<Integer, Integer>> expected) {
+        DiskFragmenter fragmenter = new DiskFragmenter();
+        List<Pair<Integer, Integer>> pairs = new ArrayList<>();
+        int from = list.size() - 1;
+        do {
+            Pair<Integer, Integer> pair = fragmenter.findFileBlockToMove(list, from);
+            pairs.add(pair);
+            from = pair.getLeft() - 1;
+
+        } while (from >= 0);
+        Assertions.assertEquals(expected, pairs);
+
+    }
 
 
 }
